d7c4cfece88db9948b08311728f5c45157a8707d
kolf <kolf@live.cn>
2018-05-02 18:39:04 +0800

    push

--- Shopping01/graphics/js/app.js
+++ Shopping01/graphics/js/app.js
@@ -1,31 +1,113 @@
-init(function(){
-  var pageBlocks = $(".js-page-block");
-  var scrollBtns = $(".js-scroll-trigger");
-  scrollBtns.click(function() {
-    var btn = $(this);
-    var target = $(this).attr("href") || $(this).attr("data-href");
-    console.log(target);
-    if (target && $(target).length) {
-      if (location.hash != target) {
-        location.hash = target;
-      }
-      scrollBtns.removeClass("active");
-      btn.addClass("active");
-      pageBlocks.removeClass("d-flex");
-      $(target).addClass("d-flex");
-    }
-    return false;
+(function() {
+  var $buyList = $("#buyList");
+  var productList = PRODUCTS.list.map(function(item) {
+    return {
+      buyNum: 0,
+      id: item.id,
+      title: item.title,
+      price: item.price,
+      size: item.size,
+      imgUrl: item.imgUrl
+    };
   });
-});
 
-function init(callback) {
-  var target = location.hash;
+  init(function() {
+    // 执行滚动方法
+    (function() {
+      var pageBlocks = $(".js-page-block");
+      var scrollBtns = $(".js-scroll-trigger");
+      scrollBtns.click(function() {
+        var btn = $(this);
+        var target = $(this).attr("href") || $(this).attr("data-href");
 
-  if(target){
-    $(target).addClass("d-flex");
-  }else{
-    $(".js-page-block").eq(0).addClass("d-flex")
+        if (target && $(target).length) {
+          if (location.hash != target) {
+            location.hash = target;
+          }
+          scrollBtns.removeClass("active");
+          btn.addClass("active");
+          pageBlocks.removeClass("d-flex");
+          $(target).addClass("d-flex");
+        }
+        return false;
+      });
+    })();
+
+    // 执行添加到购物车
+    (function() {
+      var btn = $(".js-buy-btn");
+      btn.click(function() {
+        var id = $(this).data("id");
+        buy(id, "+1");
+      });
+    })();
+
+    (function() {
+      $("#checkout").click(function() {
+        console.log(this);
+      });
+    })();
+  });
+
+  function init(callback) {
+    var target = location.hash;
+
+    if (target) {
+      $(target).addClass("d-flex");
+    } else {
+      $(".js-page-block")
+        .eq(0)
+        .addClass("d-flex");
+    }
+
+    callback();
   }
 
-  callback();
-}
+  function buy(id, num) {
+    var product = productList.find(function(item) {
+      return item.id == id;
+    });
+
+    var buyHtml = [];
+
+    if (num === "+1") {
+      product.buyNum++;
+    } else if (num === "-1") {
+      product.buyNum--;
+    } else {
+      product.buyNum = num;
+    }
+
+    var productIndex = 0;
+    productList.forEach(function(item) {
+      if (item.buyNum > 0) {
+        productIndex++;
+        buyHtml.push(
+          "<tr><td>" +
+            productIndex +
+            '</td><td class="text-center"><img src="' +
+            item.imgUrl +
+            '" alt="' +
+            item.title +
+            '" width="160" class="img-full"></td><td><p>' +
+            item.title +
+            "</p><p>" +
+            item.size +
+            "</p></td><td>" +
+            item.price +
+            '</td><td><input type="number" value="' +
+            item.buyNum +
+            '" class="form-control text-center"></td></tr>'
+        );
+      }
+    });
+
+    if (buyHtml.length > 0) {
+      $buyList.html(buyHtml.join(""));
+    } else {
+      $buyList.html(
+        '<tr><td class="text-center" colspan="5">您的购物车还没有产品...</td></tr>'
+      );
+    }
+  }
+})();
--- Shopping01/graphics/js/loadHtml.js
+++ Shopping01/graphics/js/loadHtml.js
@@ -1,6 +1,6 @@
 var APP_KEY = "shopping01";
 
-$(document).ready(function () {
+$(document).ready(function() {
   var page = $("#page");
   var path = "[%GraphicsPath()%]";
   var navHtml = [],
@@ -9,112 +9,125 @@ $(document).ready(function () {
     productHtml = [],
     productsHtml = [];
 
-
   navHtml.push(
     '<li class="nav-item nav-title"><a class="nav-link js-scroll-trigger" href="#' +
-    APP_KEY +
-    PRODUCTS.id +
-    '">' +
-    PRODUCTS.name +
-    "</a></li>"
+      APP_KEY +
+      PRODUCTS.id +
+      '">' +
+      PRODUCTS.name +
+      "</a></li>"
   );
   navHtml.push('<li class="nav-item nav-divider"></li>');
-  PRODUCTS.categorys.forEach(function (c) {
+  PRODUCTS.categorys.forEach(function(c) {
     navHtml.push(
       '<li class="nav-item nav-title"><a class="nav-link js-scroll-trigger" href="#' +
-      APP_KEY +
-      c.id +
-      '">' +
-      c.name +
-      "</a></li>"
+        APP_KEY +
+        c.id +
+        '">' +
+        c.name +
+        "</a></li>"
     );
 
     categoryHtml.push(
       '<div class="col-lg-4 col-md-6 mb-4"><div class="card pt-3 js-scroll-trigger" data-href="#' +
-      APP_KEY +
-      c.id +
-      '"><div class="card-body"><h4 class="card-title">' +
-      c.title +
-      '</h4><p class="card-text">' +
-      c.subTitle +
-      '</p></div><img class="card-img-top" src="' +
-      c.imgUrl +
-      '"></div></div>'
+        APP_KEY +
+        c.id +
+        '"><div class="card-body"><h4 class="card-title">' +
+        c.title +
+        '</h4><p class="card-text">' +
+        c.subTitle +
+        '</p></div><img class="card-img-top" src="' +
+        c.imgUrl +
+        '"></div></div>'
     );
 
     sectionHtml.push(
       '<div class="page-section p-3 p-lg-5 h-100 mx-auto flex-column bg-white js-page-block" id="' +
-      APP_KEY +
-      c.id +
-      '"><div class="my-auto"><div class="row"><div class="col-8"><div class="p-3 p-lg-5"><div class="text-center mb-5"><h2 class="text-warning">' +
-      c.title +
-      "</h2><h3>" +
-      c.subTitle +
-      "</h3></div>" +
-      c.content +
-      '</div></div><div class="col-4"><img src="' +
-      c.imgUrl +
-      '" class="img-fluid" alt=""></div></div></div></div>'
+        APP_KEY +
+        c.id +
+        '"><div class="my-auto"><div class="row"><div class="col-8"><div class="p-3 p-lg-5"><div class="text-center mb-5"><h2 class="text-warning">' +
+        c.title +
+        "</h2><h3>" +
+        c.subTitle +
+        "</h3></div>" +
+        c.content +
+        '</div></div><div class="col-4"><img src="' +
+        c.imgUrl +
+        '" class="img-fluid" alt=""></div></div></div></div>'
     );
   });
   navHtml.push(
     '<li class="nav-item nav-divider"></li><li class="nav-item nav-title"><a class="nav-link js-scroll-trigger" href="#onlineShop">Online-Shop</a></li><li class="nav-item nav-divider"></li>'
   );
-  PRODUCTS.list.forEach(function (l) {
+  PRODUCTS.list.forEach(function(l) {
     navHtml.push(
       '<li class="nav-item nav-title"><a class="nav-link js-scroll-trigger" href="#' +
-      APP_KEY +
-      l.id +
-      '">' +
-      l.name +
-      "</a></li>"
+        APP_KEY +
+        l.id +
+        '">' +
+        l.name +
+        "</a></li>"
     );
 
     sectionHtml.push(
       '<div class="page-section p-3 p-lg-5 h-100 mx-auto flex-column js-page-block" id="' +
-      APP_KEY +
-      l.id +
-      '" style="background-image: url(../../graphics/img/FTE_image.jpg)"><div class="my-auto"><div class="row"><div class="col-7 offset-5"><div class="bg-white p-lg-5 h-100"><div class="text-center mb-5"><h2 class="text-warning">'+l.title+'</h2><h3>'+l.subTitle+'</h3></div>' +
-      l.content +
-      "</div></div></div></div></div>"
+        APP_KEY +
+        l.id +
+        '" style="background-image: url(../../graphics/img/FTE_image.jpg)"><div class="my-auto"><div class="row"><div class="col-7 offset-5"><div class="bg-white p-lg-5 h-100"><div class="text-center mb-5"><h2 class="text-warning">' +
+        l.title +
+        "</h2><h3>" +
+        l.subTitle +
+        "</h3></div>" +
+        l.content +
+        "</div></div></div></div></div>"
     );
 
     productsHtml.push(
-      '<div class="col"><div class="card mb-4 box-shadow"><img class="card-img-top" src="../../graphics/img/skin_reset essence.png"><div class="card-body"><h5 class="card-title product-title">SKIN RESET ESSENCE</h5><p class="card-text">Pre-Serum 15,000円 30mL</p><div class=""><button type="button" class="btn btn-sm btn-secondary">BUY</button><button type="button" class="btn btn-sm btn-secondary js-scroll-trigger" data-href="#Bestsellers">DETAIL</button></div></div></div></div>'
+      '<div class="col"><div class="card mb-4 box-shadow"><img class="card-img-top" src="' +
+        l.imgUrl +
+        '"><div class="card-body"><h5 class="card-title product-title">' +
+        l.title +
+        '</h5><p class="card-text">' +
+        l.size +
+        '</p><div class="btns"><button type="button" class="btn btn-sm btn-secondary js-buy-btn" data-id="' +
+        l.id +
+        '">BUY</button><button type="button" class="btn btn-sm btn-secondary js-scroll-trigger" data-href="#' +
+        APP_KEY +
+        l.id +
+        '">DETAIL</button></div></div></div></div>'
     );
   });
 
   sectionHtml.push(
     '<div class="page-section p-3 p-lg-5 h-100 mx-auto flex-column js-page-block" id="onlineShop"><div class="_my-auto"><div class="row text-center">' +
-    productsHtml.join('') +
-    "</div></div></div>"
+      productsHtml.join("") +
+      "</div></div></div>"
   );
 
   sectionHtml.unshift(
     '<div class="page-section h-100 mx-auto p-3 p-lg-5 flex-column js-page-block" id="' +
-    APP_KEY +
-    PRODUCTS.id +
-    '"><div class="my-auto row text-center"><div class="col-sm-8"><div class="row">' +
-    categoryHtml.join('') +
-    '</div></div><div class="col-sm-4"><div class="my-auto"><h3 class="p-3">' +
-    PRODUCTS.title +
-    "</h3>" +
-    PRODUCTS.content +
-    "</div></div></div></div>"
+      APP_KEY +
+      PRODUCTS.id +
+      '"><div class="my-auto row text-center"><div class="col-sm-8"><div class="row">' +
+      categoryHtml.join("") +
+      '</div></div><div class="col-sm-4"><div class="my-auto"><h3 class="p-3">' +
+      PRODUCTS.title +
+      "</h3>" +
+      PRODUCTS.content +
+      "</div></div></div></div>"
   );
-
 
   var bodyHtml =
     '<nav class="navbar navbar-expand-lg _bg-white navbar-dark bg-dark _bg-primary fixed-top" id="sideNav"><a class="navbar-brand js-scroll-trigger" href="#page-top"><span class="d-block d-lg-none">' +
     PRODUCTS.name +
     '</span></a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse" id="navbarSupportedContent"><ul class="navbar-nav"><li class="nav-item nav-title"><a class="nav-link js-scroll-trigger" href="#home"><i class="fa fa-home icon-x2" style="position: relative;top:4px"></i> HOME</a></li><li class="nav-item nav-divider"></li>' +
-    navHtml.join('') +
+    navHtml.join("") +
     '<li class="nav-item nav-divider"></li><li class="nav-item"><a class="nav-link js-scroll-trigger" href="#myCart"> REVIEW SHOPPING CART<p><i class="fa fa-shopping-cart icon-x2"></i></p></a></li></ul></div></nav><div class="container-fluid p-0">' +
-    sectionHtml.join('') +
-    '<div class="page-section p-3 p-lg-5 h-100 mx-auto flex-column bg-white js-page-block" id="myCart"><div class="my-auto"><div class="pb-3"><h3><i class="fa fa-shopping-cart icon-x2"></i> MY CART (2)</h3></div><div class="table-responsive"><table class="table table-hover"><thead><tr><th>#</th><th>PRODUCT</th><th>ITEM</th><th>PRCE</th><th>QUANTITY</th></tr></thead><tbody><tr><td>1</td><td class="text-center"><img src="../../graphics/img/category1.png" alt="" width="160" class="img-full"></td><td>FIRST TIME ESSENCE xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</td><td>$140</td><td><input type="number" value="1" class="form-control textt-center"></td></tr><tr><td>1</td><td class="text-center"><img src="../../graphics/img/category1.png" alt="" width="160" class="img-full"></td><td>FIRST TIME ESSENCExxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</td><td>$140</td><td><input type="number" value="1" class="form-control textt-center"></td></tr><tr><td>1</td><td class="text-center"><img src="../../graphics/img/category1.png" alt="" width="160" class="img-full"></td><td>FIRST TIME ESSENCE xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</td><td>$140</td><td><input type="number" value="1" class="form-control textt-center"></td></tr><tr><td>1</td><td class="text-center"><img src="../../graphics/img/category1.png" alt="" width="160" class="img-full"></td><td>FIRST TIME ESSENCExxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</td><td>$140</td><td><input type="number" value="1" class="form-control textt-center"></td></tr></tbody></table></div><div class="text-right"><span class="display-4">$290</span><div><button class="btn btn-primary">CHECKOUT</button></div></div></div></div></div><link href="../../graphics/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"><link href="../../graphics/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet"><link href="../../graphics/css/app.css" rel="stylesheet"><script src="../../graphics/vendor/bootstrap/js/bootstrap.bundle.min.js"></script><script src="../../graphics/vendor/jquery-easing/jquery.easing.min.js"></script><script src="../../graphics/js/app.js"></script>';
+    sectionHtml.join("") +
+    '<div class="page-section p-3 p-lg-5 flex-column bg-white js-page-block" id="myCart"><div class="pb-3"><h3><i class="fa fa-shopping-cart icon-x2"></i> MY CART (<span id="total">0</span>)</h3></div><div class="table-responsive"><table class="table table-hover"><thead><tr><th>#</th><th>PRODUCT</th><th>ITEM</th><th>PRCE</th><th>QUANTITY</th></tr></thead><tbody id="buyList"><tr><td class="text-center" colspan="5">您的购物车还没有产品...</td></tr></tbody></table></div><div class="text-right"><span class="display-4" id="totalPrice">$0</span><div><button class="btn btn-primary" id="checkout">CHECKOUT</button></div></div></div></div><link href="../../graphics/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"><link href="../../graphics/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet"><link href="../../graphics/css/app.css" rel="stylesheet"><script src="../../graphics/vendor/bootstrap/js/bootstrap.bundle.min.js"></script><script src="../../graphics/vendor/jquery-easing/jquery.easing.min.js"></script><script src="../../graphics/js/app.js"></script>';
 
   if (page.length > 0) {
-    $("link").each(function () {
+    $("link").each(function() {
       $(this).remove();
     });
 
