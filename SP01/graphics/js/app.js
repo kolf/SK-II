f02bb5f95b670d375dbbad9f0afc2bcfbf5b07ae
kolf <kolf@live.cn>
2018-05-09 13:55:28 +0800

    动态创建变量

---
+++ SP01/graphics/js/app.js
@@ -0,0 +1,130 @@
+(function () {
+  var $buyList = $("#buyList"),
+    $total = $("#total"),
+    $totalPrice = $("#totalPrice"),
+    $cartTotal = $("#cart-num");
+  var productList = PRODUCTS
+    .list
+    .map(function (item) {
+      return {
+        buyNum: 0,
+        id: item.id,
+        title: item.title,
+        price: item.price,
+        size: item.size,
+        imgUrl: item.imgUrl,
+        added: 0
+      };
+    });
+
+  init(function () {
+    // 执行滚动方法
+    (function () {
+      var pageBlocks = $(".js-page-block");
+      var scrollBtns = $(".js-scroll-trigger");
+      scrollBtns.click(function () {
+        var btn = $(this);
+        var target = $(this).attr("href") || $(this).attr("data-href");
+
+        if (target && $(target).length) {
+          if (location.hash != target) {
+            location.hash = target;
+          }
+          scrollBtns.removeClass("active");
+          btn.addClass("active");
+          pageBlocks.removeClass("d-flex");
+          $(target).addClass("d-flex");
+        }
+        return false;
+      });
+    })();
+
+    // 执行添加到购物车
+    (function () {
+      var btn = $(".js-buy-btn");
+      btn.on('click', function () {
+        var id = $(this).data("id");
+        buy(id, "+1");
+      });
+    })();
+
+    // 移除购物车
+    (function () {
+      var list = $("#buyList");
+      list.on('click', function (e) {
+        var target = e.target;
+        if (/js\-detele\-btn/.test(target.className)) {
+          var id = $(target).parents('tr').data('id');
+          buy(id, 0)
+        }
+      });
+    })();
+
+    (function () {
+      $("#checkout")
+        .click(function () {
+          console.log(this);
+        });
+    })();
+  });
+
+  function init(callback) {
+    var target = location.hash;
+
+    if (target) {
+      $(target).addClass("d-flex");
+    } else {
+      $(".js-page-block")
+        .eq(0)
+        .addClass("d-flex");
+    }
+
+    callback();
+  }
+
+  function buy(id, num) {
+    var product = productList.find(function (item) {
+      return item.id == id;
+    });
+
+    var buyHtml = [];
+
+    if (num === "+1") {
+      product.buyNum++;
+    } else if (num === "-1") {
+      product.buyNum--;
+    } else {
+      product.buyNum = num;
+    }
+
+    var productIndex = 0,
+      totalPrice = 0;
+    productList.forEach(function (item) {
+      var added = item.added > 0 ? 1 : 0;
+      if (added) {
+        productIndex++;
+        totalPrice += item.buyNum * item.price;
+        buyHtml.push('<tr data-id="' + item.id + '"><td>' + productIndex + '</td><td class="text-center"><img src="' + item.imgUrl + '" alt="' + item.title + '" width="160" class="img-full"></td><td><div>' + item.title + '</div><p>' + item.size + '</p><p><button type="button" class="btn btn-sm btn-danger js-detele-btn">DETELE</button></p></td><td>$' + item.price + '</td><td><input type="number" value="' + item.buyNum + '" class="form-control text-center"></td></tr>');
+      }
+
+      $('input[name=' + APP_KEY + '_item' + id + 'Added]').val(added);
+      $('input[name=' + APP_KEY + '_item' + id + 'Buy]').val(item.buyNum);
+    });
+
+    var total = buyHtml.length;
+    if (total > 0) {
+      $buyList.html(buyHtml.join(""));
+    } else {
+      $buyList.html('<tr><td class="text-center" colspan="5">您的购物车还没有产品...</td></tr>');
+    }
+    $totalPrice.text("$" + totalPrice);
+    $total.text(total);
+    $cartTotal.text(total);
+  }
+
+  window.Tracker = function (id) {
+    var input = $('input[name=' + APP_KEY + '_item' + id + 'Click]');
+    var value = input.val() || 0;
+    input.val(value++);
+  }
+})();
---
+++ SP01/graphics/js/loadHtml.js
@@ -0,0 +1,84 @@
+$(document)
+  .ready(function () {
+    window.APP_KEY = $('input[name*=hid_q_]').val();
+    var page = $("#page");
+    var path = "[%GraphicsPath()%]";
+    var navHtml = [],
+      top3Html = [],
+      productsHtml = [],
+      sectionHtml = [],
+      hiddenInputs = [];
+
+    navHtml.push('<li class="nav-item nav-title"><a class="nav-link">' + PRODUCTS.name + '</a></li>');
+    navHtml.push('<li class="nav-item nav-divider"></li>');
+
+    PRODUCTS
+      .list
+      .forEach(function (item, index) {
+        if (index < 3) {
+          navHtml.push('<li class="nav-item nav-title"><a class="nav-link">' + item.name + '</a></li>');
+          top3Html.push('<div class="col-4 mb-4"><div onclick="Tracker(' + item.id + ')" class="card pt-3"><div class="card-body"><h4 class="card-title">' + item.title + '</h4><p class="card-text">' + item.subTitle + '</p></div><img class="card-img-top" src="' + item.imgUrl + '"></div></div>')
+        }
+
+        sectionHtml.push('<div class="page-section p-3 p-lg-5 h-100 mx-auto flex-column js-page-block" id=' +
+          '"section' + APP_KEY + item.id + '" style="background-image: url([%GraphicsPath()%]/img/FTE_image.jpg)"><div class' +
+          '="my-auto"><div class="row"><div class="col-7 offset-5"><div class="bg-white p-l' +
+          'g-5 p-5 h-100"><div class="text-center mb-5"><h2 class="text-warning">' + item.title + "</h2><h3>" + item.subTitle + "</h3></div>" + item.content + "</div></div></div></div></div>");
+
+        productsHtml.push('<div class="col" id="item_' + item.id + '"><div class="card mb-4 box-shadow"><img class="card-img-top" src="' + item.imgUrl + '"><div class="card-body"><h5 class="card-title product-title">' + item.title + '</h5><p class="card-text">' + item.size + '</p><div class="btns"><button type="button" class="btn btn-sm btn-secondary js-b' +
+          'uy-btn" data-id="' + item.id + '">BUY</button><button type="button" onclick="Tracker(' + item.id + ')"  class="btn btn-sm btn-secondary js-scroll-trigger" data-href="#' + APP_KEY + item.id + '">DETAIL</button></div></div></div></div>');
+
+        hiddenInputs.push('<input type="hidden" name="' + APP_KEY + '_item' + item.id + 'Click" value="0" /><input type="hidden" name="' + APP_KEY + '_item' + item.id + 'Buy" value="0" /><input type="hidden" name="' + APP_KEY + '_item' + item.id + 'Added" value="0" />')
+      });
+
+    navHtml.push('<li class="nav-item nav-divider"></li><li class="nav-item nav-title js-scroll-tr' +
+      'igger" href="#onlineShop"><a class="nav-link">Online-Shop</a></li>')
+
+    sectionHtml.push('<div class="page-section p-3 p-lg-5 h-100 mx-auto flex-column js-page-block" id=' +
+      '"onlineShop"><div class="_my-auto"><div class="row text-center">' + productsHtml.join("") + '</div></div><div class="text-right"><div class="footer-checkout d-inline-block">' +
+      '<div class="text-center footer-checkout-icon"><i class="fa fa-shopping-cart"></i' +
+      '><span id="cart-num">0</span></div><button class="btn btn-primary js-scroll-trig' +
+      'ger" href="#myCart">MY CART</button></div></div></div>');
+
+    sectionHtml.unshift('<div class="page-section h-100 mx-auto p-3 p-lg-5 flex-column js-page-block" id=' +
+      '"section' + APP_KEY + PRODUCTS.id + '"><div class="my-auto row text-center"><div class="col-sm-8"><div class="row">' + top3Html.join("") + '</div></div><div class="col-sm-4"><div class="my-auto"><h3 class="p-3">' + PRODUCTS.title + "</h3>" + PRODUCTS.content + "</div></div></div></div>");
+
+    var bodyHtml = '<nav class="navbar navbar-expand-lg _bg-white navbar-dark bg-dark _bg-primary fi' +
+      'xed-top" id="sideNav"><a class="navbar-brand js-scroll-trigger" href="#page-top"' +
+      '><span class="d-block d-lg-none">' + PRODUCTS.name + '</span></a><button class="navbar-toggler" type="button" data-toggle="collapse" d' +
+      'ata-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria' +
+      '-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-ico' +
+      'n"></span></button><div class="collapse navbar-collapse" id="navbarSupportedCont' +
+      'ent"><ul class="navbar-nav"><li class="nav-item nav-title"><a class="nav-link js' +
+      '-scroll-trigger" href="#home"><i class="fa fa-home icon-x2" style="position: rel' +
+      'ative;top:4px"></i> HOME</a></li><li class="nav-item nav-divider"></li>' + navHtml.join("") + '<li class="nav-item nav-divider"></li><li class="nav-item"><a class="nav-link js' +
+      '-scroll-trigger" href="#myCart"> REVIEW SHOPPING CART<p><i class="fa fa-shopping' +
+      '-cart icon-x2"></i></p></a></li></ul></div></nav><div class="container-fluid p-0' +
+      '">' + sectionHtml.join("") + '<div class="page-section p-3 p-lg-5 flex-column bg-white js-page-block" id="myCa' +
+      'rt"><div class="pb-3"><h3><i class="fa fa-shopping-cart icon-x2"></i> MY CART (<' +
+      'span id="total">0</span>)</h3></div><div class="table-responsive"><table class="' +
+      'table table-hover"><thead><tr><th>#</th><th>PRODUCT</th><th>ITEM</th><th>PRCE</t' +
+      'h><th>QUANTITY</th></tr></thead><tbody id="buyList"><tr><td class="text-center" ' +
+      'colspan="5">您的购物车还没有产品...</td></tr></tbody></table></div><div class="text-right"' +
+      '><span class="display-4" id="totalPrice">$0</span><div><button class="btn btn-pr' +
+      'imary" id="next_button">CHECKOUT</button></div></div></div></div>' + hiddenInputs.join('') + '<script src="../../graphics/vendor/bootstrap/js/bootstrap.bundle.min.js"></scrip' +
+      't><script src="../../graphics/js/app.js"></script>';
+
+    if (page.length > 0) {
+      $("link")
+        .each(function () {
+          if (!$(this).data("af")) {
+            $(this).remove();
+          }
+        });
+
+      $("#test_mode_bar").remove();
+      $("#mobile_display").remove();
+      $("#test_options_dialog").remove();
+      $("#overlay").remove();
+      $("#submit_overlay").remove();
+      $("#page")
+        .before(bodyHtml)
+        .hide();
+    }
+  });
